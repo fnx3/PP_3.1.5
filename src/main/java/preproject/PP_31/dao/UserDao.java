837cf1c4d899704802471f4c72b1aa1bcefc463e
fnx3 <vladislav.timoshka@gmail.com>
2024-04-20 16:39:25 +0300

    change CRUD operations from DAO to Repository

--- src/main/java/preproject/PP_31/dao/UserDao.java
+++ src/main/java/preproject/PP_31/dao/UserDao.java
@@ -1,8 +1,4 @@
 package preproject.PP_31.dao;
 
-import preproject.PP_31.model.User;
-
-import java.util.List;
-
 public interface UserDao {
 }
--- src/main/java/preproject/PP_31/dao/UserDaoImp.java
+++ src/main/java/preproject/PP_31/dao/UserDaoImp.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
-import preproject.PP_31.model.User;
-
-import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao{
